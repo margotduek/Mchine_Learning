da4a8910102d35e770197efe3e83047435b9de1c
margotduek <duekis_94@hotmail.com>
2017-11-08 22:11:09 -0600

    :rocket: ya quedó

--- proyecto7/Proyecto7.py
+++ proyecto7/Proyecto7.py
@@ -4,26 +4,28 @@ import csv
 import matplotlib.pyplot as plt
 from pylab import plot, ylim
 
-
+# Funcion para encontrar el centroide mas cercano a cada punto
 def findClosestCentroids(X, initial_centroids):
-    # set_to_zero(initial_centroids)
-    clusters = []
-    cluster_size = 0
+    # arreglo para cuardar el valor de la distancia entre dos puntos de cada valor de X
+    cluster_size = len(initial_centroids)
+    clusters = [0] * cluster_size
+    # arreglo para guardar a que cluster pertenece cada putno
     cluster = []
-    for i in range(len(initial_centroids)):
-        temp = "__" + str(i)
-        clusters.append(temp)
-        cluster_size = cluster_size + 1
 
     for i in range(len(X)):
         for j in range(cluster_size):
+            # C² = a² + b²
+            # Aplicamos esta formula para cada punto y cada cluster seleccionado para ver a que cluster pertenece cada numero
             clusters[j] = np.sqrt((X[i][0] - initial_centroids[j][0])**2 + (X[i][1] - initial_centroids[j][1])**2 )
         min_num = np.amin(clusters)
+        # Metemos al arreglo cluster el cluster al que pertenece cada punto
         cluster.append(clusters.index(min_num))
     return cluster
 
 
+# Funcion para encontrar clusters mas centricos
 def computeCentroids(X, idx, K):
+    # inicializamos arreglos del tamañno de k
     totsX = [0] * K
     totsY = [0] * K
     tots = np.zeros((2, K))
@@ -32,59 +34,30 @@ def computeCentroids(X, idx, K):
     ammount = [0] * K
     for i in range(len(X)):
         cluster = idx[i]
+        # Hacemos la sumatoria de cada X y cada Y en cada cluster
         Xs[cluster] = Xs[cluster] + X[i][0]
         Ys[cluster] = Ys[cluster] + X[i][1]
+        # Vemos cuantos puntos hay en cada cluster
         ammount[cluster] = ammount[cluster] + 1
-        # print(Xs[cluster], cluster, ammount[cluster])
 
+    # Sacamos el promedio de cada cluster
     for j in range(K):
         totsX[j] = Xs[j]/ammount[j]
         totsY[j] = Ys[j]/ammount[j]
-        print(totsX[j])
     tots[0] = totsX
     tots[1] = totsY
+    return tots.transpose()
 
-
-    print(tots.transpose())
-
-    #         Xs[j]
-    #         temp = temp = "X" + str(j)
-    #         temp = temp + X[i][0]
-    # print(X0)
-    #         X0 = X0 + X[i][0]
-    #         Y0 = Y0 + X[i][1]
-    #         tot_0 = tot_0 + 1
-    #
-    #     else if(idx[i] == 1):
-    #         X1 = X1 + X[i][0]
-    #         Y1 = Y1 + X[i][1]
-    #         tot_1 = tot_1 + 1
-    #     else if(idx[i] == 0):
-    #         X2 = X2 + X[i][0]
-    #         Y2 = Y2 + X[i][1]
-    #         tot_2 = tot_2 + 1
-    # x0 = X0/tot_0
-    # y0 = Y0/tot_0
-    # x1 = X1/tot_1
-    # y1 = Y1/tot_1
-    # x2 = X2/tot_2
-    # y2 = Y2/tot_2
-
-
-
-# def runkMeans(X, initial_centroids, max_iters, true):
-    # Es el pseudocódigo Azul
-
+# Fuincion para encontrar los centroides
 def kMeansInitCentroids(X, K):
-    # permutated = np.array(X)
     centroids = []
     permutated = np.random.permutation(X)
     for i in range(K):
         centroids.append(permutated[i].tolist())
     return centroids
 
+# Leemos el archivo y guardamos los datos en una matriz
 def read_file(filename):
-    # Open the file
     text_file = open(filename, "r")
 
     lines = text_file.readlines()
@@ -105,8 +78,36 @@ def read_file(filename):
     data = np.array(data)
     return data.transpose()
 
+# Iteramos y recalculamos que cluster es el que conviene mas
+def runkMeans(X, centroids, max_iters, true):
+    idx = findClosestCentroids(X, centroids)
+    old_centroids = []
+    for ii in range(max_iters):
+        for i in range(len(X)):
+            if(idx[i] == 0):
+                plt.scatter(X[i][0], X[i][1], color = 'r')
+            if(idx[i] == 1):
+                plt.scatter(X[i][0], X[i][1], color = 'g')
+            if(idx[i] == 2):
+                plt.scatter(X[i][0], X[i][1], color = 'b')
+        for i in range(len(centroids)):
+            plt.scatter(centroids[i][0], centroids[i][1], color = 'y', s = 130, marker = 'x', linewidth = '7')
+            if(true):
+                for j in range(len(old_centroids)):
+                    plt.scatter(old_centroids[j][i][0], old_centroids[j][i][1], color = 2, s = 70, marker = 'x', linewidth = '4')
+                    if(len(centroids) > 0):
+                        plt.plot([old_centroids[j-1][i][0],old_centroids[j][i][0]], [old_centroids[j - 1][i][1], old_centroids[j][i][1]])
+
+
+        old_centroids.append(centroids)
+        plt.show()
+        centroids = computeCentroids(X, idx, 3)
+        idx = findClosestCentroids(X, centroids)
+
+
 
 x = read_file("ex7data2.txt")
 initial_centroids = kMeansInitCentroids(x, 3)
-idx = findClosestCentroids(x, initial_centroids)
-computeCentroids(x, idx, 3)
+# idx = findClosestCentroids(x, initial_centroids)
+# new_centroids = computeCentroids(x, idx, 3)
+runkMeans(x, initial_centroids, 10, True)
